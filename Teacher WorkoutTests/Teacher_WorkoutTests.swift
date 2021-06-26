b3fdae494c5695cda9badb6e7fb1923c3172891d
Mihai Pantiru <mihai.pantiru@gmail.com>
2021-06-26 09:57:15 +0300

    Initial project setup (#13)
    * Add empty project
    
    * Remove dummy generated code
    
    * Remove dummy generated code
    
    * Remove iPad support

---
+++ Teacher WorkoutTests/Teacher_WorkoutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Teacher_Workout
+
+class Teacher_WorkoutTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
---
+++ Teacher WorkoutUITests/Teacher_WorkoutUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+class Teacher_WorkoutUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
