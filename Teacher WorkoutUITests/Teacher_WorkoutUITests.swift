25a85faeee068c84e2344bfcf18cf1e0bdedf7bd
Mihai Pantiru <mihai.pantiru@gmail.com>
2021-06-26 09:42:02 +0300

    Remove dummy generated code

--- Teacher WorkoutUITests/Teacher_WorkoutUITests.swift
+++ Teacher WorkoutUITests/Teacher_WorkoutUITests.swift
@@ -7,6 +7,5 @@ class Teacher_WorkoutUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 }
