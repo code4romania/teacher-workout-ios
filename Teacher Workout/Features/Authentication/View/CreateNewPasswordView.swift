8547e562344720b77a0ae59d571a1c64e3805c82
Vlad Stanescu <vladstanescu94@gmail.com>
2021-07-13 12:06:04 +0300

    Add basic reset password and create new password screens

---
+++ Teacher Workout/Features/Authentication/View/CreateNewPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateNewPasswordView.swift
+//  Teacher Workout
+//
+//  Created by Vlad Stanescu on 13.07.2021.
+//
+
+import SwiftUI
+
+struct CreateNewPasswordView: View {
+    var body: some View {
+        Text("Create New Password")
+    }
+}
+
+struct CreateNewPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateNewPasswordView()
+    }
+}
---
+++ Teacher Workout/Features/Authentication/View/ResetPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  ResetPasswordView.swift
+//  Teacher Workout
+//
+//  Created by Vlad Stanescu on 13.07.2021.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text("Reset Password")
+    }
+}
+
+struct ResetPasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResetPasswordView()
+    }
+}
