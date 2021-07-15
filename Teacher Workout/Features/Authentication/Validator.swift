e17aa58c24173afd3b51d1046274d9aaba987982
Vlad Stanescu <vladstanescu94@gmail.com>
2021-07-15 10:58:57 +0300

    Add small comment explaining password regex

--- Teacher Workout/Features/Authentication/Validator.swift
+++ Teacher Workout/Features/Authentication/Validator.swift
@@ -17,6 +17,7 @@ class Validator {
         case .email:
             regExPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         case .password:
+            // At least 1 lowercase character 1 uppercase character and 1 number, minimum 8 characters, max 15 characters.
             regExPattern = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$"
         }
 
