815dd4e7117007d29ffe32eee024ee886f8bb8fb
Mihai Pantiru <mihai.pantiru@gmail.com>
2021-08-04 14:32:37 +0300

    [Home] Fix search bar height

--- Teacher Workout/Components/General/SearchBar.swift
+++ Teacher Workout/Components/General/SearchBar.swift
@@ -10,7 +10,7 @@ struct SearchBar: View {
             TextField(AppStrings.Search.placeholder.localized(), text: $text)
                 .foregroundColor(Color("AccentColor"))
                 .font(Font.custom("Mulish-SemiBold", size: 16))
-                .frame(height: 40)
+                .frame(height: 34)
                 .padding(7)
                 .padding(.horizontal, 25)
                 .background(Color(.white))
