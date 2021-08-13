ce795524a27ebe93469abcf757cfb8ad18e71ca8
Mihai Garda Popescu <mihaigpm@gmail.com>
2021-08-13 11:28:44 +0300

    Add UI tests support with ViewInspector and create a first test

--- Teacher WorkoutUITests/Teacher_WorkoutUITests.swift
+++ Teacher WorkoutUITests/Teacher_WorkoutUITests.swift
@@ -1,4 +1,8 @@
+@testable import Teacher_Workout
+import ViewInspector
 import XCTest
+
+extension LessonIntroView: Inspectable {}
 
 class Teacher_WorkoutUITests: XCTestCase {
     override func setUpWithError() throws {
@@ -6,4 +10,25 @@ class Teacher_WorkoutUITests: XCTestCase {
     }
 
     override func tearDownWithError() throws {}
+
+    func mockedLessonObject() -> Lesson {
+        let lessonId = "1"
+        let lessonTitle = "Lesson Test Title"
+        let themeTitle = "Lesson Theme Title Test"
+        let duration = "24"
+        return Lesson(id: lessonId,
+                      title: lessonTitle,
+                      imageURL: nil,
+                      themeTitle: themeTitle,
+                      duration: duration)
+    }
+
+    func testLessonIntroViewTitle() throws {
+        let mockedLesson = mockedLessonObject()
+
+        let lessonIntroView = LessonIntroView(lesson: mockedLesson)
+
+        let title = try lessonIntroView.inspect().zStack().vStack(1).text(3).string()
+        XCTAssertEqual(title, mockedLesson.title)
+    }
 }
