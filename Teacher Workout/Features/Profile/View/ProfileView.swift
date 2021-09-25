63581b350384bd962300a6a6b5f6234bd646c07e
Vlad Stanescu <vladstanescu94@gmail.com>
2021-09-25 13:14:00 +0300

    Extract picker view

--- Teacher Workout/Features/Profile/View/ProfileView.swift
+++ Teacher Workout/Features/Profile/View/ProfileView.swift
@@ -1,25 +1,11 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @State private var isResultsSelected: Bool = false
-
     var body: some View {
         GeometryReader { geometry in
             VStack(spacing: 0) {
                 ProfileHeaderView(width: geometry.size.width)
-                HStack(spacing: 0) {
-                    PickerTab(isSelected: isResultsSelected, title: AppStrings.Profile.resultsTabLabel.localized())
-                        .onTapGesture {
-                            isResultsSelected = true
-                        }
-                    PickerTab(isSelected: !isResultsSelected, title: AppStrings.Profile.settingsTabLabel.localized())
-                        .onTapGesture {
-                            isResultsSelected = false
-                        }
-                }
-                .padding(.top, 22)
-                .frame(maxWidth: .infinity)
-                .modifier(TextSemibold())
+                ProfilePickerView()
             }
         }
         .navigationBarTitle(AppStrings.Menu.profile.localized())
